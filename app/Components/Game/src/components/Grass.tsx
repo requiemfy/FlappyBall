cc0dbbe7dbcff429d4841027fe44cf060e8d32c4
valar-morghulis-bit <jasonramirez@student.laverdad.edu.ph>
2021-02-12 22:17:56 +0800

    Cleaned, preparing for adding random grass height

--- app/Components/Game/src/components/Grass.tsx
+++ app/Components/Game/src/components/Grass.tsx
@@ -15,7 +15,6 @@ interface State {
 
 type GrassObject = {
   animation: any;
-  leftVal: number;
 };
 
 export default class Grass extends React.PureComponent<Box.Props & Props, any> { // @remind update state here
@@ -23,19 +22,17 @@ export default class Grass extends React.PureComponent<Box.Props & Props, any> {
   private grassWidth = this.props.size[0];
   private grassA: GrassObject = {
     animation: null,
-    leftVal: 0,
   } 
   private grassB: GrassObject = {
     animation: null,
-    leftVal: this.props.size[0],
   } 
 
   constructor(props: Box.Props & Props) {
     super(props);
     const { gameHeight } = GameDimension.window();
     this.state = {
-      grassAleft: new Animated.Value(this.grassA.leftVal),
-      grassBleft: new Animated.Value(this.grassB.leftVal),
+      grassAleft: new Animated.Value(0),
+      grassBleft: new Animated.Value(this.grassWidth),
       grassAheight: 0.3,
       grassBheight: 0.3,
     }
@@ -45,7 +42,6 @@ export default class Grass extends React.PureComponent<Box.Props & Props, any> {
     console.log("GRASS DID MOUNT");
     this.props.setRef ? this.props.setRef(this) : null;
     Dimensions.addEventListener('change', this.orientationCallback); // luckily this will not invoke in eg. landscape left to landscape right
-    this.move(); // @remind clear
   }
 
   componentWillUnmount() {
@@ -72,86 +68,13 @@ export default class Grass extends React.PureComponent<Box.Props & Props, any> {
       duration: duration,
       easing: Easing.linear,
       useNativeDriver: !(Platform.OS === 'web'),
-      // useNativeDriver: false,
     });
   }
 
   move = () => {
-    // const moveGrassA = () => {
-    //   this.grassA.animation = this.animate(
-    //     this.state.grassAleft,
-    //     this.calcDuration(this.grassWidth, this.grassA.leftVal)
-    //   );
-    //   this.grassA.animation.start(({ finished }: any) => { // @remind refactore
-    //     if (finished) {
-    //       this.stop();
-    //       this.grassA.leftVal = this.grassB.leftVal + this.grassWidth;
-    //       this.state.grassAleft.setValue(this.grassA.leftVal);
-    //       moveGrassA();
-    //       moveGrassB();
-    //     }
-    //   });
-    // }
-    // moveGrassA();
-
-    // const moveGrassB = () => {
-    //   console.log("GRASSSSSSSSSSSSSSSSSSSSSSSSSS B: " + this.grassB.leftVal)
-    //   this.grassB.animation = this.animate(
-    //     this.state.grassBleft, // starting
-    //     this.calcDuration(this.grassWidth, this.grassB.leftVal)
-    //   );
-    //   this.grassB.animation.start(({ finished }: any) => { // @remind refactore
-    //     if (finished) {
-    //       this.stop();
-    //       this.grassB.leftVal = this.grassA.leftVal + this.grassWidth;
-          // this.state.grassBleft.setValue(this.grassB.leftVal);
-
-    //       moveGrassB();
-    //       moveGrassA();
-    //     }
-    //   });
-    // }
-    // moveGrassB();
-
-    // @remind clear
-    // USING PARALLEL
-    // const switching = (a = -this.grassWidth, b = 0)  => {
-    //   this.grassA.animation = this.animate(
-    //     this.state.grassAleft,
-    //     this.calcDuration(this.grassWidth, 0), a
-    //   );
-    //   this.grassB.animation = this.animate(
-    //     this.state.grassBleft, // starting
-    //     this.calcDuration(this.grassWidth, 0), b
-    //   );
-    // }
-
-    // const animate1 = () => {
-    //   this.state.grassAleft.setValue(0);
-    //   this.state.grassBleft.setValue(this.grassWidth);
-    //   switching();
-    //   Animated.parallel([
-    //     this.grassA.animation,
-    //     this.grassB.animation
-    //   ]).start(({ finished }: any) => finished ? animate2() : null);
-    // }
-    // const animate2 = () => {
-    //   this.state.grassAleft.setValue(this.grassWidth);
-    //   this.state.grassBleft.setValue(0);
-    //   switching(0, -this.grassWidth);
-    //   Animated.parallel([
-    //     this.grassA.animation,
-    //     this.grassB.animation
-    //   ]).start(({ finished }: any) => finished ? animate1() : null);
-    // }
-    // animate1();
-
-
-    // @remind refactoring
     const switching = (toValue=[-this.grassWidth, 0], left=[0, this.grassWidth])  => {
       this.state.grassAleft.setValue(left[0]);
       this.state.grassBleft.setValue(left[1]);
-
       this.grassA.animation = this.animate(
         this.state.grassAleft,
         this.calcDuration(this.grassWidth, 0), toValue[0]
@@ -161,22 +84,6 @@ export default class Grass extends React.PureComponent<Box.Props & Props, any> {
         this.calcDuration(this.grassWidth, 0), toValue[1]
       );
     }
-
-    // const animate1 = () => {
-    //   switching();
-    //   Animated.parallel([
-    //     this.grassA.animation,
-    //     this.grassB.animation
-    //   ]).start(({ finished }: any) => finished ? animate2() : null);
-    // }
-    // const animate2 = () => {
-    //   switching([0, -this.grassWidth], [this.grassWidth, 0]);
-    //   Animated.parallel([
-    //     this.grassA.animation,
-    //     this.grassB.animation
-    //   ]).start(({ finished }: any) => finished ? animate1() : null);
-    // }
-    // animate1();
 
     let first = "grass-a"
     const animateGrass = () => {
@@ -193,23 +100,18 @@ export default class Grass extends React.PureComponent<Box.Props & Props, any> {
       ]).start(({ finished }: any) => finished ? animateGrass() : null);
     }
     animateGrass()
-
   }
 
   stop = () => {
-    // this.state.grassAleft.stopAnimation((value: number) => this.grassA.leftVal = value);
-    // this.state.grassBleft.stopAnimation((value: number) => this.grassB.leftVal = value);
-    // @remind clear
-    console.log("STOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOPPPP GRASSSSS")
-    this.grassA.animation.stop()
-    this.grassB.animation.stop()
+    this.grassA.animation?.stop()
+    this.grassB.animation?.stop()
   }
 
   render() {
     return (
       <Box.default {...this.props}>
-        <Leaves left={this.state.grassAleft} randHeight={this.state.grassAheight} myColor={"red"} {...this.props}/>
-        <Leaves left={this.state.grassBleft} randHeight={this.state.grassBheight} myColor={"yellow"} {...this.props}/>
+        <Leaves left={this.state.grassAleft} randHeight={this.state.grassAheight} myColor={"transparent"} {...this.props}/>
+        <Leaves left={this.state.grassBleft} randHeight={this.state.grassBheight} myColor={"transparent"} {...this.props}/>
       </Box.default>
     )
   }
